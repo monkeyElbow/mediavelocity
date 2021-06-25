e97ebf54a15b90e5510a1d2c8f068e535d2cae61
James Gerhold <james@mediavelocity.com>
2021-06-25 10:20:04 -0500

    Update PhotosConklin.js

--- src/components/PhotosConklin.js
+++ src/components/PhotosConklin.js
@@ -19,11 +19,11 @@ export const PhotosConklin = [
       width: 6,
       height: 4
     },
-    {
-      src: "https://pro2-bar-s3-cdn-cf1.myportfolio.com/9c0c32ffbfb6bec1105e5a0038d455f6/2d388a43b0ea32cd7bb12085_rw_1200.jpg?h=2ae8bec92173878ea62832044e5a223a",
-      width: 6,
-      height: 4
-    },
+    // {
+    //   src: "https://pro2-bar-s3-cdn-cf1.myportfolio.com/9c0c32ffbfb6bec1105e5a0038d455f6/2d388a43b0ea32cd7bb12085_rw_1200.jpg?h=2ae8bec92173878ea62832044e5a223a",
+    //   width: 6,
+    //   height: 4
+    // },
     {
       src: "https://pro2-bar-s3-cdn-cf.myportfolio.com/9c0c32ffbfb6bec1105e5a0038d455f6/8be83ab95ba03244dfc9dfcf_rw_1200.jpg?h=6da510faffb335a2cd0070d2acb90c71",
       width: 6,
@@ -34,11 +34,11 @@ export const PhotosConklin = [
       width: 6,
       height: 4
     },
-    {
-      src: "https://pro2-bar-s3-cdn-cf4.myportfolio.com/9c0c32ffbfb6bec1105e5a0038d455f6/522db39efffd1019ff1790a7_rw_1200.jpg?h=bf4bb3c4a59e504b72b70f5f4c90d21d",
-      width: 6,
-      height: 4
-    },
+    // {
+    //   src: "https://pro2-bar-s3-cdn-cf4.myportfolio.com/9c0c32ffbfb6bec1105e5a0038d455f6/522db39efffd1019ff1790a7_rw_1200.jpg?h=bf4bb3c4a59e504b72b70f5f4c90d21d",
+    //   width: 6,
+    //   height: 4
+    // },
     {
       src: "https://pro2-bar-s3-cdn-cf1.myportfolio.com/9c0c32ffbfb6bec1105e5a0038d455f6/c3fe72f92b62dc736e7810ea_rw_1200.jpg?h=cfea0f0985fd586b6a525c35542dff4e",
       width: 6,
